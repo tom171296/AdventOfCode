062965b920c63c8bf0cbdb7e7e8caee9853a89fa
Tom van den Berg <tom171296@users.noreply.github.com>
2024-12-01 14:39:51 +0000

    clean up 2024

--- src/Days/Day1.cs
+++ src/Days/Day1.cs
@@ -9,44 +9,6 @@ public class Day01 : AbstractDay
     {
         var lines = GetInput();
 
-        var sum = 0;
-        for (int i = 0; i < lines.Length; i++)
-        {
-            var line = lines[i];
-
-            line = line.Replace("zero", "zer0ero");
-            line = line.Replace("one", "on1ne");
-            line = line.Replace("two", "tw2wo");
-            line = line.Replace("three", "th3hree");
-            line = line.Replace("four", "fou4our");
-            line = line.Replace("five", "fiv5ive");
-            line = line.Replace("six", "si6ix");
-            line = line.Replace("seven", "seve7even");
-            line = line.Replace("eight", "eigh8ight");
-            line = line.Replace("nine", "nin9ine");
-
-            var first = -1;
-            var last = -1;
-
-            for (int j = 0; j < line.Length; j++)
-            {
-                if (line[j] > '0' && line[j] <= '9')
-                {
-                    var parsedNumber = line[j] - '0';
-                    if (first == -1)
-                    {
-                        first = parsedNumber;
-                    }
-
-                    last = parsedNumber;
-                }
-
-            }
-            
-            var number = first * 10 + last;
-            sum += number;
-        }
-
-        Result(sum);
+        
     }
 }
